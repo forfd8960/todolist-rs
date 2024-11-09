f431a4a640cb14d27c71b3ba58df703dac9c32a0
forfd8960 <forfd8960@gmail.com>
2024-11-09 18:26:36 +0800

    feat: UT for models - user

--- src/handlers/user.rs
+++ src/handlers/user.rs
@@ -4,7 +4,7 @@ use sqlx::{FromRow, PgPool};
 
 use crate::errors::AppError;
 
-#[derive(Debug, Clone, FromRow, Serialize, Deserialize)]
+#[derive(Debug, Clone, FromRow, Serialize, Deserialize, PartialEq)]
 pub struct User {
     pub id: i64,
     pub username: String,
@@ -58,4 +58,98 @@ pub async fn get_user_by_id(id: i64, pool: &PgPool) -> Result<Option<User>, AppE
             .fetch_optional(pool)
             .await?;
     Ok(user)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use anyhow::Result;
+    use sqlx::PgPool;
+
+    #[tokio::test]
+    async fn test_create_user_works() -> Result<()> {
+        let pool =
+            PgPool::connect("postgres://db_manager:super_admin8801@localhost:5432/todolist_test")
+                .await?;
+
+        sqlx::migrate!("./migrations").run(&pool).await?;
+
+        let user = create_user(
+            &CreateUser {
+                username: "Bob".to_string(),
+                email: "bob@acme.org".to_string(),
+                password_hash: "test".to_string(),
+            },
+            &pool,
+        )
+        .await?;
+
+        assert_eq!(user.username, "Bob".to_string());
+        assert_eq!(user.email, "bob@acme.org".to_string());
+        assert_eq!(user.password_hash, None);
+
+        sqlx::query("TRUNCATE TABLE users,todos;")
+            .execute(&pool)
+            .await?;
+
+        Ok(())
+    }
+
+    #[tokio::test]
+    async fn test_get_user_by_email_work() -> Result<()> {
+        let pool =
+            PgPool::connect("postgres://db_manager:super_admin8801@localhost:5432/todolist_test")
+                .await?;
+
+        sqlx::migrate!("./migrations").run(&pool).await?;
+
+        let user = create_user(
+            &CreateUser {
+                username: "Bob".to_string(),
+                email: "bob@acme.org".to_string(),
+                password_hash: "test".to_string(),
+            },
+            &pool,
+        )
+        .await?;
+
+        let user2 = get_user_by_email("bob@acme.org", &pool).await?;
+        assert!(user2.is_some());
+        assert_eq!(user, user2.unwrap());
+
+        sqlx::query("TRUNCATE TABLE users,todos;")
+            .execute(&pool)
+            .await?;
+
+        Ok(())
+    }
+
+    #[tokio::test]
+    async fn test_get_user_by_id_work() -> Result<()> {
+        let pool =
+            PgPool::connect("postgres://db_manager:super_admin8801@localhost:5432/todolist_test")
+                .await?;
+
+        sqlx::migrate!("./migrations").run(&pool).await?;
+
+        let user = create_user(
+            &CreateUser {
+                username: "Bob".to_string(),
+                email: "bob@acme.org".to_string(),
+                password_hash: "test".to_string(),
+            },
+            &pool,
+        )
+        .await?;
+
+        let user2 = get_user_by_id(user.id, &pool).await?;
+        assert!(user2.is_some());
+        assert_eq!(user, user2.unwrap());
+
+        sqlx::query("TRUNCATE TABLE users,todos;")
+            .execute(&pool)
+            .await?;
+
+        Ok(())
+    }
 }

7c88132488788e097c0af2df83342245afc6d545
forfd8960 <forfd8960@gmail.com>
2024-11-09 21:50:55 +0800

    feat: UT for models - Todo

--- src/errors.rs
+++ src/errors.rs
@@ -4,12 +4,19 @@ use thiserror::Error;
 pub enum AppError {
     #[error("user email already been used: {0}")]
     EmailAlreadyExists(String),
+
     #[error("user auth failed: {0}")]
     AuthFailed(String),
+
     #[error("load config err: {0}")]
     TomlError(#[from] toml::de::Error),
+
     #[error("db error: {0}")]
     DBError(#[from] sqlx::Error),
+
     #[error("io error: {0}")]
     IOError(#[from] std::io::Error),
+
+    #[error("todo not exists: {0}")]
+    TodoNotExists(String),
 }

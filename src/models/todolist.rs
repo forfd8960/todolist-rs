7c0ed2c3b4c8915a8e03e4381b64e14b87dba64d
forfd8960 <forfd8960@gmail.com>
2024-11-10 08:56:07 +0800

    feat: UT for models - Todo

---
+++ src/models/todolist.rs
@@ -0,0 +1,257 @@
+use chrono::{DateTime, Utc};
+use serde::{Deserialize, Serialize};
+use sqlx::{FromRow, PgPool};
+
+use crate::errors::AppError;
+
+#[derive(Debug, FromRow, Serialize, Deserialize)]
+pub struct Todo {
+    pub id: i64,
+    pub user_id: i64,
+    pub title: String,
+    pub description: String,
+    pub status: i16,
+    pub created_at: DateTime<Utc>,
+    pub updated_at: DateTime<Utc>,
+}
+
+#[derive(Debug)]
+pub enum TodoStatus {
+    Pending,
+    Ready,
+    InProgress,
+    Done,
+}
+
+impl TodoStatus {
+    pub fn get_status_id(&self) -> i16 {
+        match self {
+            TodoStatus::Pending => 0,
+            TodoStatus::Ready => 1,
+            TodoStatus::InProgress => 3,
+            TodoStatus::Done => 3,
+        }
+    }
+}
+
+#[derive(Debug, Deserialize)]
+pub struct CreateTodo {
+    pub user_id: i64,
+    pub title: String,
+    pub description: String,
+    pub status: i16,
+}
+
+pub async fn create_todo(input: &CreateTodo, pool: &PgPool) -> Result<Todo, AppError> {
+    let todo = sqlx::query_as(
+        r#"INSERT INTO todos (user_id,title,description,status)
+                VALUES ($1,$2,$3,$4)
+                RETURNING id,user_id,title,description,status,created_at,updated_at;
+                "#,
+    )
+    .bind(input.user_id)
+    .bind(&input.title)
+    .bind(&input.description)
+    .bind(input.status)
+    .fetch_one(pool)
+    .await?;
+
+    Ok(todo)
+}
+
+pub async fn get_todo_by_id(id: i64, pool: &PgPool) -> Result<Option<Todo>, AppError> {
+    let todo = sqlx::query_as(
+        r#"
+        SELECT id,user_id,title,description,status,created_at,updated_at
+        FROM todos
+        WHERE id=$1;
+    "#,
+    )
+    .bind(id)
+    .fetch_optional(pool)
+    .await?;
+
+    Ok(todo)
+}
+
+pub async fn get_todos_by_user_id(user_id: i64, pool: &PgPool) -> Result<Vec<Todo>, AppError> {
+    let todos = sqlx::query_as(
+        r#"
+        SELECT id,user_id,title,description,status,created_at,updated_at
+        FROM todos
+        WHERE user_id=$1
+        ORDER BY created_at DESC;
+    "#,
+    )
+    .bind(user_id)
+    .fetch_all(pool)
+    .await?;
+
+    Ok(todos)
+}
+
+pub async fn update_todo_status(
+    id: i64,
+    status: i16,
+    pool: &PgPool,
+) -> Result<Option<Todo>, AppError> {
+    let todo = get_todo_by_id(id, pool).await?;
+    if todo.is_none() {
+        return Err(AppError::TodoNotExists(format!("{} is not exists", id)));
+    }
+
+    let todo: Todo = sqlx::query_as(
+        r#"
+        UPDATE todos SET status=$1 WHERE id=$2
+        RETURNING id,user_id,title,description,status,created_at,updated_at;
+    "#,
+    )
+    .bind(status)
+    .bind(id)
+    .fetch_one(pool)
+    .await?;
+
+    Ok(Some(todo))
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::models::user::{create_user, CreateUser};
+
+    use super::*;
+    use anyhow::Result;
+
+    #[tokio::test]
+    async fn test_create_todo() -> Result<()> {
+        let pool =
+            PgPool::connect("postgres://db_manager:super_admin8801@localhost:5432/todolist_test")
+                .await?;
+
+        sqlx::migrate!("./migrations").run(&pool).await?;
+
+        let user = create_user(
+            &CreateUser {
+                username: "Bob".to_string(),
+                email: "bob@acme.org".to_string(),
+                password_hash: "test".to_string(),
+            },
+            &pool,
+        )
+        .await?;
+
+        let todo = create_todo(
+            &CreateTodo {
+                user_id: user.id,
+                title: "axum-tutorial".to_string(),
+                description: "1: write doc, 2: make video".to_string(),
+                status: TodoStatus::Pending.get_status_id(),
+            },
+            &pool,
+        )
+        .await?;
+        assert_eq!(todo.title, "axum-tutorial".to_string());
+        assert_eq!(todo.description, "1: write doc, 2: make video".to_string());
+        assert_eq!(todo.status, TodoStatus::Pending.get_status_id());
+        assert_eq!(todo.user_id, user.id);
+
+        sqlx::query("TRUNCATE TABLE users,todos;")
+            .execute(&pool)
+            .await?;
+
+        Ok(())
+    }
+
+    #[tokio::test]
+    async fn test_get_todo_by_id() -> Result<()> {
+        let pool =
+            PgPool::connect("postgres://db_manager:super_admin8801@localhost:5432/todolist_test")
+                .await?;
+
+        sqlx::migrate!("./migrations").run(&pool).await?;
+
+        let user = create_user(
+            &CreateUser {
+                username: "Bob".to_string(),
+                email: "bob@acme.org".to_string(),
+                password_hash: "test".to_string(),
+            },
+            &pool,
+        )
+        .await?;
+
+        let todo = create_todo(
+            &CreateTodo {
+                user_id: user.id,
+                title: "axum-tutorial".to_string(),
+                description: "1: write doc, 2: make video".to_string(),
+                status: TodoStatus::Pending.get_status_id(),
+            },
+            &pool,
+        )
+        .await?;
+
+        let todo_res = get_todo_by_id(todo.id, &pool).await?;
+        assert!(todo_res.is_some());
+
+        let todo = todo_res.unwrap();
+        assert_eq!(todo.title, "axum-tutorial".to_string());
+        assert_eq!(todo.description, "1: write doc, 2: make video".to_string());
+        assert_eq!(todo.status, TodoStatus::Pending.get_status_id());
+        assert_eq!(todo.user_id, user.id);
+        assert_eq!(todo.id, todo.id);
+
+        sqlx::query("TRUNCATE TABLE users,todos;")
+            .execute(&pool)
+            .await?;
+
+        Ok(())
+    }
+
+    #[tokio::test]
+    async fn test_update_todo_status() -> Result<()> {
+        let pool =
+            PgPool::connect("postgres://db_manager:super_admin8801@localhost:5432/todolist_test")
+                .await?;
+
+        sqlx::migrate!("./migrations").run(&pool).await?;
+
+        let user = create_user(
+            &CreateUser {
+                username: "Bob".to_string(),
+                email: "bob@acme.org".to_string(),
+                password_hash: "test".to_string(),
+            },
+            &pool,
+        )
+        .await?;
+
+        let todo = create_todo(
+            &CreateTodo {
+                user_id: user.id,
+                title: "axum-tutorial".to_string(),
+                description: "1: write doc, 2: make video".to_string(),
+                status: TodoStatus::Pending.get_status_id(),
+            },
+            &pool,
+        )
+        .await?;
+
+        let new_status = TodoStatus::Ready.get_status_id();
+
+        let todo_res = update_todo_status(todo.id, new_status, &pool).await?;
+        assert!(todo_res.is_some());
+
+        let todo = todo_res.unwrap();
+        assert_eq!(todo.title, "axum-tutorial".to_string());
+        assert_eq!(todo.description, "1: write doc, 2: make video".to_string());
+        assert_eq!(todo.status, new_status);
+        assert_eq!(todo.user_id, user.id);
+        assert_eq!(todo.id, todo.id);
+
+        sqlx::query("TRUNCATE TABLE users,todos;")
+            .execute(&pool)
+            .await?;
+
+        Ok(())
+    }
+}
---
+++ src/models/user.rs
@@ -0,0 +1,155 @@
+use chrono::{DateTime, Utc};
+use serde::{Deserialize, Serialize};
+use sqlx::{FromRow, PgPool};
+
+use crate::errors::AppError;
+
+#[derive(Debug, Clone, FromRow, Serialize, Deserialize, PartialEq)]
+pub struct User {
+    pub id: i64,
+    pub username: String,
+    pub email: String,
+
+    #[sqlx(default)]
+    #[serde(skip)]
+    pub password_hash: Option<String>,
+    pub created_at: DateTime<Utc>,
+    pub updated_at: DateTime<Utc>,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct CreateUser {
+    pub username: String,
+    pub email: String,
+    pub password_hash: String,
+}
+
+pub async fn create_user(input: &CreateUser, pool: &PgPool) -> Result<User, AppError> {
+    let user_result = get_user_by_email(&input.email, pool).await?;
+    if user_result.is_some() {
+        return Err(AppError::EmailAlreadyExists(format!(
+            "{}",
+            input.email.clone()
+        )));
+    }
+
+    let user = sqlx::query_as("INSERT INTO users(username, email, password_hash) VALUES($1, $2, $3) RETURNING id,username,email,created_at,updated_at")
+        .bind(&input.username)
+        .bind(&input.email)
+        .bind(&input.password_hash)
+        .fetch_one(pool)
+        .await?;
+    Ok(user)
+}
+
+pub async fn get_user_by_email(email: &str, pool: &PgPool) -> Result<Option<User>, AppError> {
+    let user =
+        sqlx::query_as("SELECT id,username,email,created_at,updated_at FROM users WHERE email=$1")
+            .bind(email)
+            .fetch_optional(pool)
+            .await?;
+    Ok(user)
+}
+
+pub async fn get_user_by_id(id: i64, pool: &PgPool) -> Result<Option<User>, AppError> {
+    let user =
+        sqlx::query_as("SELECT id,username,email,created_at,updated_at FROM users WHERE id=$1")
+            .bind(id)
+            .fetch_optional(pool)
+            .await?;
+    Ok(user)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use anyhow::Result;
+    use sqlx::PgPool;
+
+    #[tokio::test]
+    async fn test_create_user_works() -> Result<()> {
+        let pool =
+            PgPool::connect("postgres://db_manager:super_admin8801@localhost:5432/todolist_test")
+                .await?;
+
+        sqlx::migrate!("./migrations").run(&pool).await?;
+
+        let user = create_user(
+            &CreateUser {
+                username: "Bob".to_string(),
+                email: "bob@acme.org".to_string(),
+                password_hash: "test".to_string(),
+            },
+            &pool,
+        )
+        .await?;
+
+        assert_eq!(user.username, "Bob".to_string());
+        assert_eq!(user.email, "bob@acme.org".to_string());
+        assert_eq!(user.password_hash, None);
+
+        sqlx::query("TRUNCATE TABLE users,todos;")
+            .execute(&pool)
+            .await?;
+
+        Ok(())
+    }
+
+    #[tokio::test]
+    async fn test_get_user_by_email_work() -> Result<()> {
+        let pool =
+            PgPool::connect("postgres://db_manager:super_admin8801@localhost:5432/todolist_test")
+                .await?;
+
+        sqlx::migrate!("./migrations").run(&pool).await?;
+
+        let user = create_user(
+            &CreateUser {
+                username: "Bob".to_string(),
+                email: "bob@acme.org".to_string(),
+                password_hash: "test".to_string(),
+            },
+            &pool,
+        )
+        .await?;
+
+        let user2 = get_user_by_email("bob@acme.org", &pool).await?;
+        assert!(user2.is_some());
+        assert_eq!(user, user2.unwrap());
+
+        sqlx::query("TRUNCATE TABLE users,todos;")
+            .execute(&pool)
+            .await?;
+
+        Ok(())
+    }
+
+    #[tokio::test]
+    async fn test_get_user_by_id_work() -> Result<()> {
+        let pool =
+            PgPool::connect("postgres://db_manager:super_admin8801@localhost:5432/todolist_test")
+                .await?;
+
+        sqlx::migrate!("./migrations").run(&pool).await?;
+
+        let user = create_user(
+            &CreateUser {
+                username: "Bob".to_string(),
+                email: "bob@acme.org".to_string(),
+                password_hash: "test".to_string(),
+            },
+            &pool,
+        )
+        .await?;
+
+        let user2 = get_user_by_id(user.id, &pool).await?;
+        assert!(user2.is_some());
+        assert_eq!(user, user2.unwrap());
+
+        sqlx::query("TRUNCATE TABLE users,todos;")
+            .execute(&pool)
+            .await?;
+
+        Ok(())
+    }
+}
